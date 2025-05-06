f36552b2a014ec76ebff8c7f7a586419322eaf03
chrisngili <ngilichristopher@gmail.com>
2025-05-06 15:38:26 +0300

    tests passed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    i = 11
+    while i > 1:
+        i-=1
+        print(i)
+        print("Happy New Year!")
     pass
 
 def square_integers(int_list):
+    new_list =[]
+    for int in int_list:
+        square = int**2
+        new_list.append(square)
+    return new_list
     # code goes here!
     pass
 
 def fizzbuzz():
-    # code goes here!
+    for i in range(1,101):
+        if(i % 3 == 0) and (i % 5 == 0):
+            print("FizzBuzz")
+        elif(i % 3 == 0):
+            print("Fizz")
+        elif(i % 5 == 0):
+            print("Buzz")
+        else: print (i)            
     pass
